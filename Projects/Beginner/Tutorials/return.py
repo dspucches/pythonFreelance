9384ffd2165c447330a18f13ea2b15312ef7c3f1
Dominic Spucches <42140578+dspucches@users.noreply.github.com>
2023-04-23 00:13:04 -0400

    yes

---
+++ Projects/Beginner/Tutorials/return.py
@@ -0,0 +1,9 @@
+# Return python to return information from function
+# functions perform a specific task
+# have the function give information back (return) function communicating back to us using 'return'
+
+def cube(num):
+    return num*num*num # returns the value of the equation
+
+
+print(cube(3))
---
+++ Projects/Beginner/Tutorials/tutorial_1.py
@@ -0,0 +1,13 @@
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+
+def say_hi(name, age):  # defining a function - anytime we call the function we have to call the parameter
+    print("Hello " + name + ", you are " + age + ".")
+
+
+say_hi("Mike", "34")  # calling function - parameters are a piece of information we give to the function
+say_hi("Steve", "48")
+
