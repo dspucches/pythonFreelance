3540148ea623406319a7e2f6ef3d054b2902de0d
Dominic Spucches <42140578+dspucches@users.noreply.github.com>
2023-04-23 00:29:35 -0400

    yes

--- Projects/Beginner/Tutorials/ClassesAndObjects.py
+++ Projects/Beginner/Tutorials/ClassesAndObjects.py
@@ -1,10 +1,10 @@
 # class - define your own data-type - used in 99% of coding languages
 # to represent the example of student, create a class for a student is the same as making "student" a data-type
 
-class Student:
+class Student: # classes model real world objects
     # define atributes about Student
     def __init__(self, name, major, gpa, is_on_probation): #initialize function - map out attributes for student
-        self.name = name
+        self.name = name #attribute of name #self is the class name "Student"
         self.major = major
         self.gpa = gpa
         self.is_on_probation = is_on_probation
--- Projects/Beginner/Tutorials/app.py
+++ Projects/Beginner/Tutorials/app.py
@@ -4,5 +4,7 @@
 from ClassesAndObjects import Student
 
 student1 = Student("Jim", "Business", 3.1, False) # Object is creating a student object - instance of a class
+student2 = Student("Pam", "Art", 2.5, True)
 
-print(student1)
+print(student1.name)
+print(student2.name)
